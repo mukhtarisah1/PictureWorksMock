bacadedf459f15462ef1aab1e0b11b97c94ba3b4
mukhtar isah <mukhtarisah6@gmail.com>
2022-08-10 10:28:13 +0100

    adding web (get) route,creating UserController and updating composer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/user/{id}', [UserController::class, 'userPage']);
+
+
