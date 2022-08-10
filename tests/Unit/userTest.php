bfec7e5d3e02bd53eb94940ab4dc983910d7da0d
mukhtar isah <mukhtarisah6@gmail.com>
2022-08-10 10:56:37 +0100

    adding test_user_id(), test_user_comment_post()

--- tests/Unit/userTest.php
+++ tests/Unit/userTest.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Tests\TestCase;
 
 class userTest extends TestCase
 {
@@ -11,8 +11,20 @@ class userTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_id()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/user/id');
+        $response->assertStatus(200);
+    }
+
+    public function test_user_comment_post(){
+        $this->withoutExceptionHandling();
+        $response= $this->postJson('api/user',[
+            'id'=>1,
+            'comments' =>'victory to the spartans',
+            'password'=>'720DF6C2482218518FA20FDC52D4DED7ECC043AB'
+        ]);
+        $response->assertOk();
+        $response->assertStatus(200);
     }
 }
