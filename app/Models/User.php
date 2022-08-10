3439a6f2731a12b0c65ea926f4e0d63722b608b4
mukhtar isah <mukhtarisah6@gmail.com>
2022-08-10 10:19:16 +0100

    adding migration and model files

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -32,6 +28,8 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public $timestamps= false;
 
     /**
      * The attributes that should be cast.
